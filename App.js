f1cbe8b1e44c511e8ee307f610fa4df05d30c10e
Andrew Tai (Macbook) <andrewtai.ye@gmail.com>
2022-07-02 14:27:41 +0800

    added homestack added header title and styling

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer, useNavigation, StackActions } from '@react-navigation/native';
+
+import HomeStack from "./views/HomeStack";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name={"Notes"}
+          component={HomeStack}
+          options={{
+            headerTitle: "Note-ify",
+            headerTitleStyle: {
+              fontWeight: "bold",
+              fontSize: 30,
+            },
+            headerStyle: {
+              backgroundColor: 'yellow',
+              height: 110,
+              borderBottomWidth: 1,
+              borderBottomColor: 'black',
+            }
+          }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ views/HomeStack.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer, useNavigation, StackActions } from '@react-navigation/native';
+
+export default function HomeStack() {
+  return (
+    <View style={styles.container}>
+      <Text>This is the home page</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
